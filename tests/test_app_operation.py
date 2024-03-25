c58aaaca69cb214c93e5c6470792a4fdabd3a0e4
Abishek183 <av739@njit.edu>
2024-03-24 23:23:57 -0400

    test case

---
+++ tests/test_app_operation.py
@@ -0,0 +1,69 @@
+import pytest
+
+from app import App
+
+def test_app_get_environment_variable():
+    app = App()
+#   Retrieve the current environment setting
+    current_env = app.get_environment_variable('ENVIRONMENT')
+    # Assert that the current environment is what you expect
+    assert current_env in ['DEVELOPMENT', 'TESTING', 'PRODUCTION'], f"Invalid ENVIRONMENT: {current_env}"
+
+def test_app_start_add(capfd, monkeypatch):
+    """Test how the REPL handles an add command before exiting."""
+    # Simulate user entering an unknown command followed by 'exit'
+    inputs = iter(['add', '4', '5', 'exit'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    app = App()
+    
+    with pytest.raises(SystemExit) as excinfo:
+        app.start()
+
+    captured = capfd.readouterr()
+    assert "9" in captured.out
+
+
+def test_app_start_subtract(capfd, monkeypatch):
+    """Test how the REPL handles an add command before exiting."""
+    # Simulate user entering an unknown command followed by 'exit'
+    inputs = iter(['sub', '4', '2', 'exit'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    app = App()
+    
+    with pytest.raises(SystemExit) as excinfo:
+        app.start()
+
+    captured = capfd.readouterr()
+    assert "2" in captured.out
+
+
+def test_app_start_mul(capfd, monkeypatch):
+    """Test how the REPL handles an add command before exiting."""
+    # Simulate user entering an unknown command followed by 'exit'
+    inputs = iter(['mul', '3', '2', 'exit'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    app = App()
+    
+    with pytest.raises(SystemExit) as excinfo:
+        app.start()
+
+    captured = capfd.readouterr()
+    assert "6" in captured.out
+
+
+def test_app_start_div(capfd, monkeypatch):
+    """Test how the REPL handles an add command before exiting."""
+    # Simulate user entering an unknown command followed by 'exit'
+    inputs = iter(['div', '6', '2', 'exit'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    app = App()
+    
+    with pytest.raises(SystemExit) as excinfo:
+        app.start()
+
+    captured = capfd.readouterr()
+    assert "3" in captured.out
